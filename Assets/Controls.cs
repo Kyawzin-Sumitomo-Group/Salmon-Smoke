f0e94dc68c4a9b4b12bb9a51aee562d0b20e0d32
Eric Bowden <tcghyit@gmail.com>
2014-07-31 21:10:46 -0600

    added player

---
+++ Assets/Controls.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controls : MonoBehaviour {
+
+	public float moveSensitivity = 10;
+	public float rotateSensitivity = 300;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mouseYDelta = Input.GetAxis ("Mouse Y");
+		float mouseXDelta = Input.GetAxis ("Mouse X");
+
+		this.rigidbody2D.velocity = Vector2.up * mouseYDelta * moveSensitivity;
+		this.rigidbody2D.angularVelocity = mouseXDelta * rotateSensitivity;
+
+	}
+}
