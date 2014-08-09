c9395d18b9197dcd5d53e07dc379af7edb7bd6fe
oddballbutler <oddballbutler@gmail.com>
2014-08-09 17:35:18 -0400

    Added player collision script

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollision : MonoBehaviour {
+
+	private GameObject gm;
+	private GameManager gmScript;
+
+	// Use this for initialization
+	void Start () {
+		gm = GameObject.FindGameObjectWithTag ("GameController");
+		gmScript = gm.GetComponent<GameManager> ();
+		Debug.Log (gm);
+		Debug.Log (gmScript);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D (Collision2D col) {
+		if (col.gameObject.tag != "Boundaries") {
+			Debug.Log("Collision!!!!");
+			gmScript.DestroyPiece();
+			gmScript.NewPiece();
+		}
+	}
+}
