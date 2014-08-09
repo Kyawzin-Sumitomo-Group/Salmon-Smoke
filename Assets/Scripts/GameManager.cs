cff9673586f8ca0a20d3a58988e7e55d173cff6b
oddballbutler <oddballbutler@gmail.com>
2014-08-09 17:35:53 -0400

    Added Game Manager Script
    Right now it can destroy the player and respawn the player as a diamond
    instead.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void NewPiece() {
+		GameObject go = (GameObject)Instantiate(Resources.LoadAssetAtPath("Assets/Prefabs/PlayerDiamond.prefab", typeof(GameObject)));
+		go.transform.parent = Player.transform;
+	}
+	public void DestroyPiece() {
+		GameObject.Destroy (Player.transform.GetChild (0).gameObject);
+	}
+}
