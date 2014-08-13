867302d4abeff95ea1f89419d2d198a0ef774f02
Eric Bowden <bjamese@gmail.com>
2014-08-12 19:05:03 -0600

    added camera follow and created a level

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -5,19 +5,39 @@ public class Controls : MonoBehaviour {
 
 	public float moveSensitivity = 10;
 	public float rotateSensitivity = 300;
+	public float hspeed = 30;
 
-	// Use this for initialization
+	private float mouseYDelta;
+	private float mouseXDelta;
+
+
+
+	// Use this for initialization to ref other objects
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		float mouseYDelta = Input.GetAxis ("Mouse Y");
-		float mouseXDelta = Input.GetAxis ("Mouse X");
 
-		this.rigidbody2D.velocity = Vector2.up * mouseYDelta * moveSensitivity;
+	//graphics and controls here
+	void Update () {
+		mouseYDelta = Input.GetAxis ("Mouse Y");
+		mouseXDelta = Input.GetAxis ("Mouse X");
+
+	}
+
+	// physics calcs here
+	void FixedUpdate () {
+
+
+		//rotation
 		this.rigidbody2D.angularVelocity = mouseXDelta * rotateSensitivity;
+
+		//is addForce better to use?
+
+		//left/right
+		this.rigidbody2D.AddForce(Vector2.right * hspeed);
+
+		//up/down
+		this.rigidbody2D.velocity = Vector2.up * mouseYDelta * moveSensitivity;
 
 	}
 }
